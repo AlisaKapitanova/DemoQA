ac5996b5201ca6a05aae86874c0cedf63f08d24a
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2023-11-01 11:53:43 +0100

    Nv/test (#1)
    * Add changes to first test
    
    * Add first test changes

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,1 +1,7 @@
-hello
+describe('', () => {
+    it('Tools QA returns to mainPage', () => {
+        cy.visit("https://demoqa.com/");
+        cy.get("header a").should("have.attr", 'href', 'https://demoqa.com');
+    });
+    
+})
