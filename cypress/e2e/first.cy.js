2242be987df32bf2bed5929a76c63447ae13a4c7
AlisaKapitanova <alisa.kapitanovaplt@gmail.com>
2023-11-01 18:23:44 +0100

    CheckBox

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -3,5 +3,13 @@ describe('', () => {
         cy.visit("https://demoqa.com/");
         cy.get("header a").should("have.attr", 'href', 'https://demoqa.com');
     });
-    
+
+    it("Check Box", () => {
+        cy.visit('https://demoqa.com/');
+        cy.get('.card').eq(0).click()
+        cy.get('#item-1').contains('Check Box').click()
+        cy.get('.rct-checkbox').click()
+        cy.get('#result').should('have.text', 'You have selected :homedesktopnotescommandsdocumentsworkspacereactangularveuofficepublicprivateclassifiedgeneraldownloadswordFileexcelFile')
+
+    })
 })
