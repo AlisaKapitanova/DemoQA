f0b71f1b13605c4e792f553d36d762fa799746c3
AlisaKapitanova <alisa.kapitanovaplt@gmail.com>
2023-11-02 18:55:50 +0100

    npm ci

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -15,6 +15,7 @@ describe('', () => {
     });
 
     it("Menu title", () => {
+        
         let title = ["Elements", "Forms", "Alerts, Frame & Windows", "Widgets", "Interactions", "Book Store Application"]
 
         cy.get(".card").each((el, ind) => {
