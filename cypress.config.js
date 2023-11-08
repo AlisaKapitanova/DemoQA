29f69a79b45536039c1a493d8dd82c5f61eb266f
AlisaKapitanova <alisa.kapitanovaplt@gmail.com>
2023-11-08 11:31:30 +0100

    reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
+  },
+
+  video: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    embeddedScreenshots: true,
+    reportFilename: 'mochawesome',
+    reportDir: 'mochawesomeReports',
+    mochaFile: 'reports/test-results-[hash].xml',  
   },
 });
