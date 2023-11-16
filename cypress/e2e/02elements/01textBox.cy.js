31e0eecab0b67a2b9d326c0cc173e35d658f7135
Natalya Vasylyeva <natalya.vasylyeva.81@gmail.com>
2023-11-16 14:31:59 +0100

    import "../../support/globalHooks.js"

--- cypress/e2e/02elements/01textBox.cy.js
+++ cypress/e2e/02elements/01textBox.cy.js
@@ -1,11 +1,8 @@
 import HomePage from "../../pageObjects/HomePage.js";
 import elementsTextBoxData from "../../fixtures/elementsTextBox.json";
+import "../../support/globalHooks.js"
 
 const homePage = new HomePage();
-
-beforeEach(() => {
-  cy.visit("https://demoqa.com/");
-});
 
 describe("Elements  > Text Box", () => {
   it("TC_02.01.01.01 | Elements  > Text Box > Verify input fields names", () => {
