110c17c7a36f03b4457f6edc2dc25d3b3819e9a6
Natalya Vasylyeva <natalya.vasylyeva.81@gmail.com>
2023-11-16 14:53:01 +0100

    delete visit

--- cypress/e2e/02elements/01textBox.cy.js
+++ cypress/e2e/02elements/01textBox.cy.js
@@ -5,7 +5,6 @@ const homePage = new HomePage();
 
 describe("Elements  > Text Box", () => {
   it("TC_02.01.01.01 | Elements  > Text Box > Verify input fields names", () => {
-      cy.visit("https://demoqa.com/");
       homePage
         .chooseElementsCard()
         .chooseTextBoxElement()
