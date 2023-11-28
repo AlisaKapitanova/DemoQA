97c1372568adac27f8f7449cbe44715d994d0c63
Nadezhda Kulikova <kulikova0101@gmail.com>
2023-11-28 18:14:57 +0100

    Verify_Grid

---
+++ cypress/e2e/06interactions.cy.js
@@ -0,0 +1,26 @@
+
+// <reference types="cypress"/>
+
+import data from '../fixtures/selectablePageData.json'
+import SelectablePage from '../pageObjects/SelectablePage'
+import InteractionsPage from '../pageObjects/InteractionsPage'
+import HomePage from '../pageObjects/HomePage'
+
+describe ('InteracrionsPage', () => {
+    const interactionsPage = new InteractionsPage 
+    const selectablePage = new SelectablePage 
+    const homePage = new HomePage
+    
+    it ('Verify names of Grid tab cells', function() {
+    
+        homePage
+        .clickInteractionsBtn() 
+        interactionsPage
+        .clickSelectableBtn()
+        selectablePage
+        .clickGridBtn()
+        .getGridCells().each(($el,idx) => {
+            expect ($el.text()).to.be.equal(data.cellsNames[idx]); 
+        })    
+})
+})
