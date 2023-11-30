021f03ce465f665a03eb500d50cc6f9911b36562
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2023-11-30 11:47:13 +0000

    be visible added

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -2,29 +2,29 @@ import ProfilePage from "../pageObjects/ProfilePage"
 
 class LoginPage {
     elements = {
-        getUserNameInput: () => cy.get('#userName'),
+        getUserNameInput: () => cy.get('#userName').should('be.visible'),
         getPasswordInput: () => cy.get('#password'),
         getNewUserBtn: () => cy.get('#newUser'),
         getLogInBtn: () => cy.get('#login')
     };
 
     typeUserName(userName) {
-        this.elements.getUserNameInput().type(userName)
+        this.elements.getUserNameInput().should('be.visible').type(userName)
         return this
     };
 
     typePassword(password) {
-        this.elements.getPasswordInput().type(password)
+        this.elements.getPasswordInput().should('be.visible').type(password)
         return this
     };
 
     clickLoginBtn() {
-        this.elements.getLogInBtn().click()
+        this.elements.getLogInBtn().should('be.visible').click()
         return this
     };
 
     clickNewUserBtn() {
-        this.elements.getNewUserBtn().click()
+        this.elements.getNewUserBtn().should('be.visible').click()
         return this
     };
 
