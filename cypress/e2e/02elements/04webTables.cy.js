1c655d4ebb33faa69ffb07ca7b31d46d6c0a128a
Kseniya <ksenia.parfenovic@gmail.com>
2023-12-02 15:01:01 +0100

    install faker library

---
+++ cypress/e2e/02elements/04webTables.cy.js
@@ -0,0 +1,34 @@
+import ElementsWebTablesPage from "../../pageObjects/ElementsWebTablesPage";
+
+const webTablesPage = new ElementsWebTablesPage();
+
+describe("Elements  > Web Tables", () => {
+
+    it.skip("TC_02.04.01 | Elements  >  Web Tables >  Verify the possibility of adding a new employee to the table using the Registration Form", () => {
+        cy.visit('https://demoqa.com/webtables');
+
+        webTablesPage
+            .clickAddButton()
+            .addNewEmployee()
+            .clickSubmitButton()
+            // .checkEmployeeDataInTable()
+    });
+
+    // it.only("TC_02.04.01 | Elements  >  Web Tables >  Verify the possibility of adding a new employee to the table using the Registration Form", () => {
+    //     cy.visit('https://demoqa.com/webtables');
+
+    //     webTablesPage
+    //         .clickAddButton()
+    //         .addNewEmployee()
+    //         .clickSubmitButton()
+
+    //     const employeeData = webTablesPage.employeeData;
+
+    //     webTablesPage
+    //         .clickEditButton()
+    //         .cleanEmployee()
+    //         .addNewEmployee()
+    //         .clickSubmitButton()
+
+    // });
+});
