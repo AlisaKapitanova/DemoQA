1f9449cb577ed62d174c7695f922fb3e0ae8949e
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2023-12-02 00:02:36 +0000

    third commit

--- cypress/e2e/06bookStoreApplication/03profile.cy.js
+++ cypress/e2e/06bookStoreApplication/03profile.cy.js
@@ -12,19 +12,7 @@ const profilePage = new ProfilePage();
 describe('07.03 Book Store Application > Profile', () => {
     
     it("TC_07.04. | Verify Delete All Books", () => {
-        cy.login(loginPageData.userName, loginPageData.password)
-        bookStore
-            .clickProfileBtn()
-        profilePage
-            .clickDeleteAllBtn()
-        cy.on('window:alert', (str) => {
-            expect(str).to.equel('Do you want to delete all books?')
-        cy.get('#closeSmallModal-ok').should('contain', "OK")
-        })
-        //cy.get('#closeSmallModal-cancel').should('contain', "Cancel")
-        cy.on('window:alert', (str) => {
-            expect(str).to.equel("No books available in your's collection!")
-        })
+        
 
     })
 })
