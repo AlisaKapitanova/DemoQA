f3d59b7ebc0e7d4fc65b83e79ddfc3be4becb24d
[olgalex] <alex_olg@ukr.net>
2023-12-05 18:52:25 +0200

    Buttons

---
+++ cypress/e2e/02elements/05buttons.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress"/>
+import HomePage from "../../pageObjects/HomePage.js";
+import ElementsPage from "../../pageObjects/ElementsPage.js";
+import ButtonsPage from "../../pageObjects/ButtonsPage.cy.js"
+
+const homePage = new HomePage();
+const elementsPage = new ElementsPage()
+const buttonsPage = new ButtonsPage
+
+describe("Elements > Testing the buttons", () => {
+    it("TC_02.05.01|Elements > Buttons> Buttons are visible on the page", () => {
+    homePage
+    .chooseElementsCard()
+    elementsPage
+    .chooseButtonsElement()
+    buttonsPage
+    .chooseRightBtn()
+    .chooseDoubleClickBtn()
+    .chooseClickMeButton()
+   
+    
+    
+
+    
+    
+    
+
+
+
+
+
+
+   
+      
+            
+    })
+
+    })
