cbe0c0c51178903e96622ad4e1a6313c5f57b856
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2023-12-10 12:00:01 +0200

    02.05 (#45)

--- cypress/pageObjects/ButtonsPage.cy.js
+++ cypress/pageObjects/ButtonsPage.cy.js
@@ -1,8 +1,10 @@
 class ButtonsPage {
    getRightBtn = () => cy.get('#rightClickBtn');
    getDoubleClickBtn = () => cy.get('#doubleClickBtn');
-   getClickMeBtn = () => cy.get('div[class="col-12 mt-4 col-md-6"] div:nth-child(2) div:nth-child(3)')
-   getVerifyMessage = () => cy.get('#rightClickMessage')
+   getClickMeBtn = () => cy.get('div[class="col-12 mt-4 col-md-6"] div:nth-child(2) div:nth-child(3) button');
+   getVerifyMessage = () => cy.get('#rightClickMessage');
+   getVerifyClickMeMessage = () => cy.get('#dynamicClickMessage');
+   getVerifyDoublClickMessage = () => cy.get('#doubleClickMessage');
 
    chooseRightBtn() {
    this.getRightBtn().should('be.visible').and('have.text','Right Click Me')
@@ -14,17 +16,21 @@ class ButtonsPage {
     return this
    }
 
-   chooseClickMeButton() {
-    this.getClickMeBtn().should('be.visible').and('have.text','Click Me')
-    return this
-   }
-
    verifyRightBtn() {
       this.getRightBtn().rightclick()
       return this
       }
    verifyMessageRightBtn() {
       this.getVerifyMessage()
+   }
+   verifyClickMeBtn() {
+      this.getClickMeBtn().click()
+      return this
+   }
+   
+   verifyDoubleClickBtn() {
+      this.getDoubleClickBtn().dblclick()
+      return this
    }
 
    }
