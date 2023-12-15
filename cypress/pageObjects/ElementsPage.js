a374ebcf47629abd2fd32bfc09cb011caff2e678
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2023-12-15 10:13:41 +0100

    TC_02.04.02 (#47)

--- cypress/pageObjects/ElementsPage.js
+++ cypress/pageObjects/ElementsPage.js
@@ -1,12 +1,14 @@
 import ElementsTextBoxPage from "./ElementsTextBoxPage.js";
 import RadioButtonPage from "../pageObjects/RadioButtonPage.js";
-import ButtonsPage from "../pageObjects/ButtonsPage.cy.js"
+import ButtonsPage from "../pageObjects/ButtonsPage.cy.js";
+import ElementsWebTablesPage from "./ElementsWebTablesPage.js";
 
 class ElementsPage {
   getTextBoxElement = () => cy.get("#item-0");
   getRadioButtonElement = () => cy.get("#item-2");
-  getButtonElement = () => cy.get('.element-list').contains('Buttons')
- 
+  getButtonElement = () => cy.get(".element-list").contains("Buttons");
+  getWebTablesElement = () => cy.get(".element-list").contains("Web Tables");
+
   chooseTextBoxElement() {
     this.getTextBoxElement().click();
     return new ElementsTextBoxPage();
@@ -16,12 +18,15 @@ class ElementsPage {
     this.getRadioButtonElement().click();
     return new RadioButtonPage();
   }
-  chooseButtonsElement (){
-    this.getButtonElement().click()
-    return this
+  chooseButtonsElement() {
+    this.getButtonElement().click();
+    return this;
   }
 
- 
+  chooseWebTablesElement() { 
+    this.getWebTablesElement().click();
+    return new ElementsWebTablesPage();
+  }
 }
 
 export default ElementsPage;
